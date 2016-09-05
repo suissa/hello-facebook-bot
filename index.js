d50a3d4343f280628b632492af852032346b6dc8
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-09-05 17:50:38 -0300

    Added async

--- index.js
+++ index.js
@@ -7,26 +7,25 @@ const Elements = require('facebook-messenger-bot').Elements;
 
 const bot = new Bot(process.env.PAGE_ACCESS_TOKEN, process.env.VERIFICATION);
 
-bot.on('message', (message) => {
+bot.on('message', async message => {
     const {sender} = message;
-
     await sender.fetch('first_name');
 
     const out = new Elements();
-    out.add({ text: 'Hey, ' + sender.first_name + ', how are you?!' });
+    out.add({ text: `hey ${sender.first_name}, how are you!` });
 
     await bot.send(sender.id, out);
 });
 
 const options = {
-  key: fs.readFileSync('./privkey.pem'),
-  cert: fs.readFileSync('./cert.pem'),
-  ca: [
-    fs.readFileSync('./chain-1.pem'),
-    fs.readFileSync('./chain-2.pem')
-  ]
+    key: fs.readFileSync('./privkey.pem'),
+    cert: fs.readFileSync('./cert.pem'),
+    ca: [
+        fs.readFileSync('./chain-1.pem'),
+        fs.readFileSync('./chain-2.pem')
+    ]
 };
 
-https.createServer(options, app).listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+https.createServer(options, app).listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
